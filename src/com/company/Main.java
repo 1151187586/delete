d9b686c188a344f73b381b915f3ac15807b12d7a
李鹏健 <1151187586@qq.com>
2021-12-20 10:10:56 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    /**
+     * main 方法
+     * @param args
+     */
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
